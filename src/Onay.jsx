4ec2686b22329bc69ce3136a84905ad2cd934392
berkbogac-akgunes <berkb_akgunes@outlook.com>
2024-09-11 15:12:32 +0300

    add onay page css

--- src/Onay.jsx
+++ src/Onay.jsx
@@ -3,7 +3,13 @@ import "./Onay.css"
 function Onay() {
     return(
         <>
-        Sipariş Onaylandı.
+          <header className = "onay-content">
+          <h2 className = "header1">Teknolojik Yemekler</h2>
+          <div className = "info-message">
+          <h3 className = "header2">TEBRİKLER!</h3>
+          <h3 className = "header2">SİPARİŞİNİZ ALINDI!</h3>
+          </div>
+          </header>
         </>
     )
 }
